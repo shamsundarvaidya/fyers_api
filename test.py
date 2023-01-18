96f38d164d34ea4e05b70a4e3ccc743761c17bf2
shamsundar <sham4686@gmail.com>
2023-01-18 15:35:27 +0530

    fetch access token

---
+++ test.py
@@ -0,0 +1,15 @@
+
+from fyers_api import fyersModel
+from fyers_api import accessToken
+import access_token as at
+
+
+
+acc_tok = ""
+
+with open('token.txt') as tk:
+    acc_tok = tk.read()
+
+#print(acc_tok)
+
+
