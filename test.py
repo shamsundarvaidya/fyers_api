b9f6f2f63067b9624b30d831f921ca3942cdbfff
shamsundar <sham4686@gmail.com>
2023-01-31 15:31:21 +0530

    Fetch Nifty LTP quote

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from fyers_api import accessToken
 import access_token as at
 
 
+client_id = "09PDZZH120-100"
+
 
 acc_tok = ""
 
@@ -12,4 +14,9 @@ with open('token.txt') as tk:
 
 #print(acc_tok)
 
+fyers = fyersModel.FyersModel(client_id=client_id, token=acc_tok,log_path="./")
 
+data = {"symbols":"NSE:NIFTY50-INDEX"}
+nif_data = fyers.quotes(data)['d'][0]['v']
+nif_ltp = nif_data['lp']
+print(fyers.quotes(data)['d'][0]['v']['lp'])
